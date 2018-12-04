c12f41dc07c6422ba547a6889c8ff85ef7a760f2
Freddy Montes <freddymontes@gmail.com>
2018-12-03 19:40:10 -0600

    Add upload image to cloudinary

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -38,6 +38,24 @@ export default class CreateItem extends Component {
         });
     };
 
+    uploadFile = async e => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/fmontes/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        });
+    };
+
     render() {
         return (
             <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -45,17 +63,30 @@ export default class CreateItem extends Component {
                     <Form
                         onSubmit={async e => {
                             e.preventDefault();
-                            const res = await createItem()
+                            const res = await createItem();
                             Router.push({
                                 pathname: '/item',
                                 query: {
                                     id: res.data.createItem.id
                                 }
-                            })
+                            });
                         }}
                     >
-                        <Error error={error}></Error>
+                        <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor="image">
+                                Image
+                                <input
+                                    type="file"
+                                    id="image"
+                                    name="image"
+                                    placeholder="Image"
+                                    required
+                                    onChange={this.uploadFile}
+                                />
+                                {this.state.image && <img src={this.state.image} alt="Preview image" />}
+                            </label>
+
                             <label htmlFor="title">
                                 Title
                                 <input
