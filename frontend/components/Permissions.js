64955d3d82cfd729a2bc0a2a45df2b7a2103b2ad
Freddy Montes <freddymontes@gmail.com>
2018-12-15 11:23:07 -0600

    Handle permission in component state

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 import { Query } from 'react-apollo';
 import Error from './ErrorMessage';
@@ -7,15 +8,46 @@ import Table from './styles/Table';
 import SickButton from './styles/SickButton';
 
 class User extends Component {
+    static propTypes = {
+        user: PropTypes.shape({
+            name: PropTypes.string,
+            email: PropTypes.string,
+            id: PropTypes.string,
+            permission: PropTypes.array
+        }).isRequired
+    };
+    state = {
+        permissions: this.props.user.permissions
+    };
+
+    handlePermissionChange = (e) => {
+        const checkbox = e.target;
+        let updatedPermissions = [...this.state.permissions];
+
+        if (checkbox.checked) {
+            updatedPermissions.push(checkbox.value)
+        } else {
+            updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value)
+        }
+
+        this.setState({
+            permissions: updatedPermissions
+        })
+    }
+
     render() {
         const user = this.props.user;
         return (
             <tr>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                {possiblePermissions.map((permission, index) => <td key={index}>
+                {possiblePermissions.map(permission => <td key={permission}>
                     <label htmlFor={`${user.id}-permission-${permission}`}>
-                        <input type="checkbox" />
+                        <input type="checkbox"
+                        id={`${user.id}-permission-${permission}`}
+                        checked={this.state.permissions.includes(permission)}
+                        value={permission}
+                        onChange={this.handlePermissionChange} />
                     </label>
                 </td>)}
                 <td>
@@ -59,13 +91,13 @@ const Permissions = props => (
                         <tr>
                             <th>Name</th>
                             <th>Email</th>
-                            {possiblePermissions.map((permission, index) => <th key={index}>{permission}</th>)}
+                            {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            data.users.map((user, index) => <User key={index} user={user} />)
+                            data.users.map(user => <User key={user.id} user={user} />)
                         }
                     </tbody>
                 </Table>
