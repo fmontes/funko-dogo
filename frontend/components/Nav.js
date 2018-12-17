01ba6da4ec68d2bded0f38378981875bf374a50a
Freddy Montes <freddymontes@gmail.com>
2018-12-17 10:21:45 -0600

    UI for cart component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION} from '../components/Cart';
 
 const Nav = () => {
     return (
@@ -33,7 +35,11 @@ const Nav = () => {
                             <a>Sign In</a>
                         </Link>
                     )}
-                    
+                    <Mutation mutation={TOGGLE_CART_MUTATION}>
+                        {(toggleCart) => (
+                            <button onClick={toggleCart}>Cart</button>
+                        )}
+                    </Mutation>
                 </NavStyles>
             )}
         </User>
