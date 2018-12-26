8a2fb439bc6ad02dd24b7db8d822cd7c848c2138
Freddy Montes <freddymontes@gmail.com>
2018-12-25 21:04:19 -0600

    Update unit tests issue with graphql

--- frontend/__test__/Order.test.js
+++ frontend/__test__/Order.test.js
@@ -1,92 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import { format } from 'date-fns';
-import Head from 'next/head';
-import gql from 'graphql-tag';
-import formatMoney from '../lib/formatMoney';
-import Error from './ErrorMessage';
-import OrderStyles from './styles/OrderStyles';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import Order, { SINGLE_ORDER_QUERY } from '../components/Order';
+import { fakeOrder } from '../lib/testUtils';
 
-const SINGLE_ORDER_QUERY = gql`
-    query SINGLE_ORDER_QUERY($id: ID!) {
-        order(id: $id) {
-            id
-            charge
-            total
-            createdAt
-            user {
-                id
-            }
-            items {
-                id
-                title
-                description
-                price
-                image
-                quantity
-            }
-        }
+const mocks = [
+    {
+        request: { query: SINGLE_ORDER_QUERY, variables: { id: 'ord123' } },
+        result: { data: { order: fakeOrder() } }
     }
-`;
+];
 
-class Order extends React.Component {
-    static propTypes = {
-        id: PropTypes.string.isRequired
-    };
-    render() {
-        return (
-            <Query query={SINGLE_ORDER_QUERY} variables={{ id: this.props.id }}>
-                {({ data, error, loading }) => {
-                    if (error) return <Error error={error} />;
-                    if (loading) return <p>Loading...</p>;
-                    const order = data.order;
-                    return (
-                        <OrderStyles data-test="order">
-                            <Head>
-                                <title>FunkoDogo - Order {order.id}</title>
-                            </Head>
-                            <p>
-                                <span>Order ID:</span>
-                                <span>{this.props.id}</span>
-                            </p>
-                            <p>
-                                <span>Charge</span>
-                                <span>{order.charge}</span>
-                            </p>
-                            <p>
-                                <span>Date</span>
-                                <span>{format(order.createdAt, 'MMMM d, YYYY h:mm a')}</span>
-                            </p>
-                            <p>
-                                <span>Order Total</span>
-                                <span>{formatMoney(order.total)}</span>
-                            </p>
-                            <p>
-                                <span>Item Count</span>
-                                <span>{order.items.length}</span>
-                            </p>
-                            <div className="items">
-                                {order.items.map(item => (
-                                    <div className="order-item" key={item.id}>
-                                        <img src={item.image} alt={item.title} />
-                                        <div className="item-details">
-                                            <h2>{item.title}</h2>
-                                            <p>Qty: {item.quantity}</p>
-                                            <p>Each: {formatMoney(item.price)}</p>
-                                            <p>SubTotal: {formatMoney(item.price * item.quantity)}</p>
-                                            <p>{item.description}</p>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </OrderStyles>
-                    );
-                }}
-            </Query>
+describe('<Order/>', () => {
+    it('renders the order', async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={mocks}>
+                <Order id="ord123" />
+            </MockedProvider>
         );
-    }
-}
-
-export default Order;
-export { SINGLE_ORDER_QUERY };
+        await wait();
+        wrapper.update();
+        const order = wrapper.find('div[data-test="order"]');
+        expect(toJSON(order)).toMatchSnapshot();
+    });
+});
